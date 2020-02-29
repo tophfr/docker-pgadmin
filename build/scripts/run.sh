d471d3bf22a7625d112cfef328406beefb4e927f
Christophe GOUTEIX <gouteix@brocelia.fr>
2020-02-29 17:48:10 +0100

    fixing MASTER_PASSWORD_REQUIRED option

--- build/scripts/run.sh
+++ build/scripts/run.sh
@@ -2,13 +2,18 @@
 
 set -e
 
-if [ ! -f /data/config/pgadmin4.db ]; then
-    su-exec pgadmin python /usr/local/lib/python3.8/site-packages/pgadmin4/setup.py
-    su-exec pgadmin python /scripts/setup-server.py
+PGADMIN_PATH="/usr/local/lib/python3.8/site-packages/pgadmin4"
+CONFIG_FILE="$PGADMIN_PATH/config_local.py"
 
-    if [ "$MASTER_PASSWORD_REQUIRED" != '' ]; then
-      echo -e "\n\nMASTER_PASSWORD_REQUIRED = $MASTER_PASSWORD_REQUIRED" >> /usr/local/lib/python3.8/site-packages/pgadmin4/config_local.py
+if [ ! -f /data/config/pgadmin4.db ]; then
+    su-exec pgadmin python "$PGADMIN_PATH/setup.py"
+    su-exec pgadmin python /scripts/setup-server.py
+fi
+
+if [ "$MASTER_PASSWORD_REQUIRED" != '' ]; then
+    if ! grep ^MASTER_PASSWORD_REQUIRED "$CONFIG_FILE" >/dev/null; then
+      echo -e "\n\nMASTER_PASSWORD_REQUIRED = $MASTER_PASSWORD_REQUIRED" >> "$CONFIG_FILE"
     fi
 fi
 
-exec su-exec pgadmin python /usr/local/lib/python3.8/site-packages/pgadmin4/pgAdmin4.py
+exec su-exec pgadmin python "$PGADMIN_PATH/pgAdmin4.py"
